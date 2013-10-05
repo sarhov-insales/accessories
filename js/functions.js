ac9090878ec2264a054f319788dcfa2a0250f711
Hovhannes Sargsyan <sarhov@sarhov.com>
2013-10-06 03:33:56 +0400

    the end of top menu - Cross-browser ready

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,9 @@
-jQuery(document).ready(function($) {
-	
+$(document).ready(function() {
+	var $loginOpen = $('#logIn');
+	var $loginBox = $('#loginDropdown');
+	$loginOpen.click(function() {
+		$(this).toggleClass('active');
+		$loginBox.toggleClass('opened');
+		return false;
+	});
 });
